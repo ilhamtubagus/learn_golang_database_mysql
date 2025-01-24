da56610d34c3167c289fb2aa8b648f4d6bf36172
ilhamarfian <ilham.arfian@btpn.com>
2025-01-24 08:54:43 +0700

    data types in go

--- database.go
+++ database.go
@@ -6,7 +6,8 @@ import (
 )
 
 func GetConnection() (*sql.DB, error) {
-	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/learn_golang_database")
+	// set parseTime to true to parse datetime field automatically into time.Time
+	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/learn_golang_database?parseTime=true")
 	if err != nil {
 		return nil, err
 	}
--- sql_test.go
+++ sql_test.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestExecSql(t *testing.T) {
@@ -66,6 +67,62 @@ func TestQuerySql(t *testing.T) {
 
 		fmt.Println("id", id)
 		fmt.Println("name", name)
+	}
+
+}
+
+func TestQuerySqlComplex(t *testing.T) {
+	db, err := GetConnection()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}(db)
+
+	ctx := context.Background()
+
+	script := "SELECT id, name, email, balance, rating, birth_date, married, created_at FROM customer"
+	rows, err := db.QueryContext(ctx, script) // QueryContext can be used for query
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer func(rows *sql.Rows) {
+		err := rows.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}(rows)
+
+	for rows.Next() {
+		var id, name string
+		var email sql.NullString // to prevent error when value is NULL
+		var balance int32
+		var rating float64
+		var createdAt time.Time
+		var birthDate sql.NullTime // to prevent error when value is NULL
+		var married bool
+		err := rows.Scan(&id, &name, &email, &balance, &rating, &birthDate, &married, &createdAt)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		fmt.Println("--- Customer ---")
+		fmt.Println("id", id)
+		fmt.Println("name", name)
+		if email.Valid {
+			fmt.Println("email", email.String)
+		}
+		fmt.Println("balance", balance)
+		fmt.Println("rating", rating)
+		birthDateTime, _ := birthDate.Value()
+		fmt.Println("birth_date", birthDateTime)
+		fmt.Println("married", married)
+		fmt.Println("created_at", createdAt)
 	}
 
 }
